2dedd9aaef19480564b241da97ed54a4f5db2da1
OSNOVAwd <abdurahmanovmurodzon95@gmail.com>
2024-03-13 10:13:56 +0500

    js

--- app.js
+++ app.js
@@ -4,16 +4,32 @@ const random = ['Abdurahmanov Murodjon', 'Ahmadjanov Ismatillo', 'Ahmadjanov Val
 
 
 const generateName = () => {
-
 let result = random[Math.floor(Math.random() * random.length)]
 document.getElementById('name').textContent = result
 }
 
-document.getElementById('generate-btn').addEventListener('click', generateName)
- const name = document.getElementById('name')
- if(random[0] && random[1] && random[2] && random[3] && random[5] && random[6] && random[7] && random[8] && random[9] && random[10] && random[11] && random[12] && random[13] && random[14] && random[15]
-   ){
-    const name = document.getElementById('name').style.color = 'blue'
- }else{
-  document.getElementById('name').style.color = 'red'
- }
+const gnrBtn = document.getElementById('generate-btn')
+
+
+
+gnrBtn.addEventListener('click', function() {
+  setTimeout(generateName, 1300)
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
